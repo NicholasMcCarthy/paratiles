7b632333a7951fe12824defda78c692b98f99a34
Nicholas McCarthy <nicholas.mccarthy@gmail.com>
2013-07-01 17:10:53 +0100

    Fixed matlabpool bug, script_FE saves each data struct in temp folder and writes them at end ..

--- scripts/script_FeatureExtraction.m
+++ scripts/script_FeatureExtraction.m
@@ -13,13 +13,16 @@
 % Features extracted
 
 % 
+env.temp_dir = '/home/nick/git/paratiles/temp/';
 
 %% SETUP
 
 images = getFiles(env.image_dir, 'Suffix', '.tif', 'Wildcard', '.8.tif');          % Wildcard so it selects the large .SCN layer image
 masks = getFiles(env.image_dir, 'Suffix', '.tif', 'Wildcard', 'mask-PT.gs');
- 
-output_dir = strcat(env.dataset_dir, 'fe.', date, '/');
+
+WILDCARD = 'fe.CICM'; % date;
+
+output_dir = strcat(env.dataset_dir, WILDCARD, '-', date, '/');
 
 if ~exist(output_dir, 'dir')        % If no directory for this date
     fprintf('Output directory %s does not exist. Creating it.\n', output_dir);
@@ -59,7 +62,9 @@ D_length = 2079159;         % Preallocating number of rows when blockproc'ing 20
 % profile off;
 % profile report;
 % 
-% % matlabpool
+
+% Ensure your local setup allows this ..
+matlabpool local 4
 
 %% Histogram feature set
 
@@ -92,8 +97,8 @@ entropy_label = {'entropy'};
 
 %========================
 
-functions = { cicm_func haralick_func_lab };
-labels = [ cicm_labels haralick_labels_lab ];
+functions = { cicm_func } % haralick_func_lab };
+labels = [ cicm_labels ]; %haralick_labels_lab ];
 
 FE = FeatureExtractor(functions, labels);
 
@@ -105,30 +110,47 @@ func_fe = FE.BlockProcHandle;
 %% RUN
 
 % profile on;
+% data = zeros(D_length, length(FE.Features));  % Pre-allocate on an image
+% by image basis rather than the entire set .. Running out of memory. 
 
-data = zeros(D_length, length(FE.Features));
-row_idx = 1;
+% row_idx = 1;
    
 for i = 1:length(images)
     
     imagepath = images{i};
+    imageinfo = imfinfo(images{i});
+ 
     fprintf('Current Image: %s \n', imagepath);
     
+    % Get number of blocks processed in this image
+    numBlocks = ceil( (imageinfo.Width) / tilesize ) * ceil( (imageinfo.Height) / tilesize);
+    
+    % Pre-allocate 'data' struct
+    data = zeros(numBlocks, length(FE.Features));
+    
+    % Blockproc
     FV = blockproc(imagepath, [tilesize tilesize], func_fe);
     
+    % Reshape from image to feature vector
     FV = reshape(FV, size(FV, 1) * size(FV, 2), size(FV, 3));   
     
-    row_end = row_idx + size(FV,1)-1;       % End row of new data to be allocated
-    
-    data(row_idx:row_end ,:) = FV;          % Allocate new data ..
-    
-    row_idx = row_idx + length(FV);         % Update row_idx 
+%     % There is a single data struct for each image, so the lines below are
+%     % not needed .. 
+    %row_end = row_idx + size(FV,1)-1;       % End row of new data to be allocated
+    %data(row_idx:row_end ,:) = FV;          % Allocate new data ..
+    %row_idx = row_idx + length(FV);         % Update row_idx 
+
+    data = FV;
     
     message = num2str(any(FV));
     
     title = strcat('Matlab Processing:  ', num2str(i), '/', num2str(length(images)));
     sendmail('nicholas.mccarthy@gmail.com', title, message);
 
+    % save 'data' struct as .mat file on an image by image basis
+    matfile = strcat(env.temp_dir, 'image-', num2str(i), '_temp_data.mat');
+    save(matfile, 'data');
+    
 end
 
 % profile off;
@@ -136,8 +158,40 @@ end
 
 %% OUTPUT 
 
-writeMatrixToCSV(data, FE.Features, output_dir);
+% % Generate single column csvs with column names as filenames
+% for h = 1:length(FE.Features)
+%     filename = strcat(output_dir, FE.Features{h}, '.csv');
+%     fid = fopen(filename, 'w+');
+%     fclose(fid);
+% end
 
+for i = 1:length(images)  % for each image
+    
+    fprintf('image %d \n', i);
+    
+    matfile = strcat(env.temp_dir, 'image-', num2str(i), '_temp_data.mat');
+    load(matfile);                                                                                                      % loads 'data' struct
+    
+    for c = 1:size(data, 2)                                                                                         % Append each column
+        
+        filename = strcat(output_dir, FE.Features{c}, '.csv');                                       % Column filename 
+        fid = fopen(filename, 'a');                                                                               % Open to append
+        
+        for r = 1:size(data, 1)                                                                                     %  For each row in 'data'
+            
+            fprintf(fid,   '%0.9f\n', data(r, c));                                                               % append it to file
+            
+        end
+        fclose(fid);
+        
+    end
+  
+end
+
+
+% This function works for a single data matrix, not appending to multiple
+% ones :( 
+% writeMatrixToCSV(data, FE.Features, output_dir);
 
 %% CLEANUP
 
