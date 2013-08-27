59386c5183becb66210c6aef144997127f74d5d1
Nicholas McCarthy <nicholas.mccarthy@gmail.com>
2013-08-27 20:29:36 +0100

    Generate image dataset scripts (matlab, matlab function and python) finished and working ..

--- datasets/gen_imagedataset.py
+++ datasets/gen_imagedataset.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
-
 # Generate datasets by splitting on unique filenames.csv entries (i.e. each image)
-
+# Inputs: 	feature directory(s) 
+#			name of image (must be in supplied filenames csv)
+#  			labels file
+#			filenames file
+#			assign-ids boolean (add row numbers to dataset )
 from os import listdir
 from os.path import isfile,isdir,join
 import fnmatch
@@ -9,21 +12,20 @@ import argparse
 import re
 import random
 
-# Inputs: 	feature directory(s) 
-#			output file 			
-#  			
-#
-#
 
 # Parsing stdin args to script
 p = argparse.ArgumentParser(description="Read directory to list and class to generate dataset for", prog="gen_dataset.py")
-p.add_argument('-dir', nargs = '+', required=True, help='Directory of column csv files')                                                               # Requires a directory location
-p.add_argument('-output', required = True, type=argparse.FileType('wb', 0), help='Name of output dataset.csv')				  # The output file
-p.add_argument('-labels', required=True, type=argparse.FileType('r'), help='Column csv of labels')					  # The labels file 
-p.add_argument('-filenames', required=True, type=argparse.FileType('r'), help='Column csv of filenames')					  # The filenames file 
+p.add_argument('-dir', nargs = '+', required=True, help='Directory of column csv files')                                         # Requires a directory location
+p.add_argument('-image', nargs=1, required=True, help='The image dataset to generate.')										     # An image listed in the filenames file ..
+p.add_argument('-labels', required=True, type=argparse.FileType('r'), help='Column csv of labels')					 			 # The labels file 
+p.add_argument('-filenames', required=True, type=argparse.FileType('r'), help='Column csv of filenames')					     # The filenames file 
 p.add_argument('-assign-ids', nargs=1, required=False, help='Assign sequential numbers to each image set.')
+p.add_argument('-assign-classes', dest='assign-classes', action='store_true', help='Assign classes from labels file (limited to just classes in image) or assign all labels.')
+p.add_argument('-no-assign-classes', dest='assign-classes', action='store_false', help='Assign classes from labels file (limited to just classes in image) or assign all labels.')
 
-# args = vars(p.parse_args('-dir /home/nick/git/paratiles/datasets/HARALICK.features -class G5 G3 TIS -labels /home/nick/git/paratiles/datasets/class.info/labels.csv -output test.csv -limit-obs 5000 -assign-zeros NON'.split()));
+# args = vars(p.parse_args('-dir /home/nick/git/paratiles/datasets/HARALICK.features -image PCRC-BIMS_656-10S-B_B1-HE.scn -labels /home/nick/git/paratiles/datasets/class.info/labels.csv -no-assign-classes -filenames /home/nick/git/paratiles/datasets/class.info/filenames.csv '.split()));
+
+# args = vars(p.parse_args('-dir /home/nick/git/paratiles/datasets/HARALICK.features -image PCRC-BIMS_656-10S-B_A1-HE.scn -labels /home/nick/git/paratiles/datasets/class.info/labels.csv -filenames /home/nick/git/paratiles/datasets/class.info/filenames.csv '.split()));
 
 args = vars(p.parse_args())
 
@@ -45,12 +47,17 @@ if parse_error:
 	print "Exiting .. "
 	os.system('exit')
 
+mydirs = args['dir']
+mylabels = args['labels']
+myimage = args['image'][0]
+myfilenames = args['filenames']
+myassignids = args['assign_ids']
+myassignclasses = args['assign-classes']
 
-
-################################################################
+###################################################
 # List all the CSV files in the specified directory
 csvfiles = []
-for mydir in args['dir']:
+for mydir in mydirs:
 	for dirc in listdir(mydir):
 		if fnmatch.fnmatch(dirc, '*.csv'):
 		  mycsv = mydir + '/' + dirc
@@ -60,7 +67,7 @@ csvfiles = sorted(csvfiles)					# Sort CSV files (makes it easier to do feature 
 
 print csvfiles
 
-################################################################
+###################################################
 # Convert filenames to CSV headers
 headers = []
 for filename in csvfiles:
@@ -68,76 +75,42 @@ for filename in csvfiles:
 	myheader = myheader[myheader.rfind('/')+1:len(myheader)]    # remove the 'path/to/file/'
 	headers.append(myheader)				       # append it to list of headers
 
-################################################################
+###################################################
 # Iterate over filenames csv / get indices of unique entries 
 
-# Gets unique entries in a list
-def f4(seq): 
-   # order preserving
-   noDupes = []
-   [noDupes.append(i) for i in seq if not noDupes.count(i)]
-   return noDupes
-
-# 0. Get each line in filenames.csv
-# 1. Get each unique entry in filenames (each image)
-# 2. Get starting and end indices for each unique entry
-# 3. Map indices to unique entries
-# 4. loop over code below with selected range(start, end) for each unique index
+def finduniq(seq):
+	noDupes = []
+	[noDupes.append(i) for i in seq if not noDupes.count(i)]
+	return noDupes
 
 # Gets all filenames from csv file
-filenames = args['filenames'].readlines();
+filenames = [f.strip() for f in myfilenames.readlines()]
 
-# Gets unique filename entries
-Uf = f4(filenames)
-# Strip trailing newline chars
-Uf = [u.strip() for u in Uf]
+start = filenames.index(myimage)
 
+idx = start
+while idx < len(filenames) and filenames[idx] == myimage:
+	idx+=1
 
-for idx in range(0, len(filenames))
-	
+end = idx
 
+indices = xrange(start, end)
+numBlocks = end - start;
 
-
-
+print "Image: ", myimage
+print "Range is", start, "-", end 
+print "Total blocks: ", numBlocks
 
 ################################################################
 # Open labels file, get indices that match the supplied classes
-
-indices = []
 labels = []
 idx = 0;
 
-for line in args['labels']:
-	if line.strip() in args['class']:
-		indices.append(idx)
-		labels.append(line.strip())
-	idx+=1
+labels = mylabels.readlines();
+labels = [labels[i].strip() for i in indices]
+mylabels.close();
 
-args['labels'].close()
-
-print "Number of obs read:", len(labels)
-
-# Set maximum number of obs of ANY class, rather than class specifics .. 
-if args['limit_obs'] is not None:
-
-	limit = int(args['limit_obs'][0])
-	sampled_idx = []
-
-	print "Maximum number of obs. will be limited to: ", limit
-
-	for C in args['class']:				# For each class
-
-		class_idx = [idx for idx,val in enumerate(labels) if val == C]   # Get list indices that match this class
-
-		if len(class_idx) > limit: 										# If it's greater than the limit
-			print "Sampling", C, "as limit is reached."
-			class_idx = random.sample(class_idx, limit) 			 	# Sample up to the max allowed 
-
-		sampled_idx = sampled_idx + class_idx							# Append the selected class_idx (sampled or otherwise) to the sampled_idx list
-
-
-	labels = [labels[i] for i in sorted(sampled_idx)]					# Technically the sorted is not needed here, but why not keep it neat .. 
-	indices = [indices[i] for i in sorted(sampled_idx)]
+print "Num labels: ", len(labels)
 
 ################################################################
 # Extracting selected indices from each file
@@ -146,7 +119,7 @@ print "Reading", str(len(csvfiles)), 'files .. Please wait.'
 
 data = []
 
-for csvfile in csvfiles:                                           # for each file
+for csvfile in csvfiles:                                            # for each file
 	
 	lines = open(csvfile, 'r').readlines()                          # read all lines
 	 
@@ -154,7 +127,7 @@ for csvfile in csvfiles:                                           # for each fi
 	i = 0;
 	for idx in indices:                                             # iterate over each selected index    
 		i += 1;
-		olines.append(lines[idx].strip())                            # append the selected indices to olines list and remember to remove the newline char
+		olines.append(lines[idx].strip())                           # append the selected indices to olines list and remember to remove the newline char
 		  
 	data.append(olines)                                             # then append all of the selected lines to the data list
 
@@ -163,30 +136,28 @@ data.append(labels)													# ARFF files will always have the label written 
 
 ################################################################
 # Writing to output file
-
 # With ARFF files, headers and labels must be specified in the file itself, so no need to handle writing labels and headers to separate files.
 
-print "Writing data to output file .. Please wait."
 
-out = args['output']
+outpath = re.sub('scn', 'arff', myimage)
+
+print "Writing data to output file", outpath, " .. Please wait."
+
+out = open(outpath, 'wb')
 
 #######################
 # Write ARFF info header 
 
-out.write('% An ARFF dataset file generated by gen_dataset_arff.py\n')
+
+out.write('% An ARFF dataset file generated by gen_imagedataset.py\n')
 out.write('% Created by: Nick McCarthy <nicholas.mccarthy@gmail.com>\n\n')
 
 #######################
 # Write @RELATION section
 
-relation_str = '@RELATION '
-for c in args['class']:
-	relation_str += c + '-'
-
-relation_str += 'data\n\n'
+relation_str = '@RELATION label-imagedata\n\n'
 
 out.write(relation_str)
-
 
 #######################
 # Write @ATTRIBUTE section
@@ -194,11 +165,16 @@ out.write(relation_str)
 for header in headers:		
 	out.write('@ATTRIBUTE ' + header + ' NUMERIC\n')
 
-class_str = '@ATTRIBUTE label {'
-for c in args['class']:
-	class_str += c + ','
+if myassignclasses == True:
+	class_str = '@ATTRIBUTE label {G3, G34, G4, G45, G5, NON, TIS}\n'
+else:
+	class_str = '@ATTRIBUTE label {'
+	myclasses = sorted(finduniq(labels))
 
-class_str = class_str[0:len(class_str)-1] + '}\n\n'					
+	for c in myclasses:
+		class_str += c + ','
+
+	class_str = class_str[0:len(class_str)-1] + '}\n\n'
 
 out.write(class_str)   # The last @ATTRIBUTE line that specifies the classes
 
@@ -210,44 +186,22 @@ out.write('@DATA \n')
 num_nonzero_removed = 0		                                               # Keeping track of how many rows were removed for having no nonzero elements
 
 for row in range(0, len(indices)):                                        # for each row in data
+
 	line = ''
+
 	for col in range(0, len(data)):                                        # for each col in data
 		line += str(data[col][row]) + ','                                   # concatenate the row feature vector
 		
 	line = line[0:len(line)-1]                                             # remove last comma
 
-	if args['assign_zeros'] is None: 	# Part for removing empty rows 
-		nonzero_elements = False	
-		for c in range(1,10):                                                  # checking if nonzero numbers are in the row string ..
-			if str(c) in line[0:line.rfind(',')]:                               # checks up to last comma (since the last value is value and may have numbers
-				nonzero_elements = True 
-				break
-		
-		# Write line to output file, except if it it has no nonzero elements 
-		if (nonzero_elements):
-			line += '\n'    
-			out.write(line)
-								
-		else:
-			num_nonzero_removed += 1
-
-	else:
-		line += '\n'
-		out.write(line)
+	line += '\n'
+	out.write(line)
 
 out.close()
 
 # Print some summary data
-
-print "Number of rows read: ", len(indices)
-
-if args['assign_zeros'] is None:
-	print "Zero-element rows removed: ", num_nonzero_removed
-else:
-	print "Zero-element rows assigned label", str(args['assign_zeros'][0])
-
 print "--------------"
-print "Dataset: \t", args['output'].name
+print "Dataset: \t", outpath
 print "Number features: " , len(data)-1
 print "Number observations: " , len(indices)-num_nonzero_removed
 print "--------------"
--- scripts/script_GenerateImageDatasets.m
+++ scripts/script_GenerateImageDatasets.m
@@ -6,98 +6,17 @@
 
 images = getFiles(env.training_image_dir, 'Wildcard', '.scn');
 
-if matlabpool('size') == 0
-    matlabpool local 4;
-end
 
-% Set up feature extractor .. 
-numlevels = [16 32 64];
-distances = [1 2 4];
+%%
 
-histogram_func_lab = @(I) extract_histogram_features(rgb2cielab(I), 'NumLevels', [16 32 64]);
-histogram_labels_lab = label_histogram_features('Channels', {'L', 'A', 'B'}, 'NumLevels', [16 32 64], 'Prefix', 'lab', 'UseStrings', true);
+feature_dirs = {'datasets/HISTOGRAM.features' 'datasets/SHAPE.features'}; %, ...
+%                 'datasets/HARALICK.features', 'datasets/CICM-r1.features'};
+            
+label_path = 'datasets/class.info/labels.csv';
+filenames_path = 'datasets/class.info/filenames.csv';
+image_path = fliplr(strtok(fliplr(images{1}), '/'));
 
-haralick_func_lab = @(I) extract_haralick_features(rgb2cielab(I), 'NumLevels', [16 64], 'Distances', [1 2 4]);
-haralick_labels_lab = label_haralick_features('Channels', {'L', 'A', 'B'}, 'NumLevels', [16 64], 'Distances', [1 2 4], 'Prefix', 'lab', 'UseStrings', true);
-
-functions = { histogram_func_lab haralick_func_lab };
-labels    = [ histogram_labels_lab haralick_labels_lab ] ;
-
-FE = FeatureExtractor(functions, labels);
-
-blockproc_func = FE.BlockProcHandle;
-
-tilesize = 256;
-
-%% 
-
-for i = 1 %:length(images)
-    
-    image_path = images{i};
-    
-    fprintf('Reading %s \n', image_path);
-    
-    % Gets path to 8th IFD (i.e. the largest .. );
-    ifd_paths = unpackTiff(image_path, 8, 1);
-    
-    FV = blockproc(ifd_paths{:}, [tilesize tilesize], blockproc_func);
-    
-    output_path = regexprep(image_path, '.scn', '.LAB-features.mat')
-    
-    save(output_path, 'FV');
-    
-end
-
-%% Alternative idea: Use existing extracted feature sets ..
-
-for i = 1:length(images);
-
-    image_path = images{i};
-    
-    fprintf('Reading %s \n', image_path);
-    
-    % Gets path to 8th IFD (i.e. the largest .. );
-    ifd_paths = unpackTiff(image_path, 8, 1);
-    
-    imageinfo = imfinfo(idf_paths{:});
-    
-    numBlocks = ceil( (imageinfo.Width) / this.Tilesize ) * ceil( (imageinfo.Height) / this.Tilesize);
-    
-    output_path = regexprep(image_path, '.scn', '.LAB-features.mat')
-    
-    
-    
-    save(output_path, 'FV');
-    
-end
-
-%% Generating filenames.csv from class-labels.mat
-
-loaded = load('datasets/class.info/class_labels.mat');
-
-Df = loaded.D.filename;
-
-Uf = unique(Df);
-
-% Replace names with .scn filenames
-repname = @(x) regexprep(x, 'mask-PT.gs.tif', 'scn');
-
-output_file = ['datasets/class.info/filenames.csv'];
-
-fid = fopen(output_file, 'wb');
-
-% No header to print .. 
-
-fprintf('Writing to %s \n', output_file);
-
-for i = 1:length(Df)
-    line = repname(Df{i});
-    
-    lines = stringsplit(line, '/');
-    
-    fprintf(fid, '%s\n', lines{end});
-end
-
-fclose(fid);
+[status cmdout] = GenerateImageDataset( env.root_dir, 'Directory', feature_dirs, 'Image', image_path, ... 
+                        'Labels', label_path, 'Filenames', filenames_path, 'AssignIDs', 0, 'AssignClasses', 1);
 
 
